31b69387cf2fe467f075be5be6378c7c962c5c06
acronimuss <zozolandian0@gmail.com>
2021-05-14 11:26:17 +0200

    PR_L10 SERVER

--- app/app.js
+++ app/app.js
@@ -18,16 +18,16 @@ app.use(express.static('public'));
 app.use(cors());
 
 mongoose.connect(config.databaseUrl, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false
+ useNewUrlParser: true,
+ useCreateIndex: true,
+ useFindAndModify: false
 }, (error) => {
-  if (error) {
-    console.error(error);
-  }
-  else {
-    console.info('Connect with database established');
-  }
+ if (error) {
+   console.error(error);
+ }
+ else {
+   console.info('Connect with database established');
+ }
 });
 
 process.on('SIGINT', () => {
